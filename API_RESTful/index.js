333bde19c857e3c4a0c068354415283e0c6ace96
hugozepeda18 <a01228679@exatec.tec.mx>
2022-03-15 17:20:28 -0600

    Ultimo commit antes de revision

--- API_RESTful/index.js
+++ API_RESTful/index.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const apiRouter = require("./api")
+const path = require("path")
+const bodyParse = require("body-parser")
 
 const PORT = 8080
 const app = express()
 
 app.use(express.json())
+app.use(bodyParse.urlencoded({ extended: false}))
 app.use("/api", apiRouter)
 
-
 app.get("/", (req, res) => {
-    res.send("Hello Server")
+    res.sendFile(path.join(__dirname, "./../html/form.html"))
 })
 
 app.listen(PORT, () => {
