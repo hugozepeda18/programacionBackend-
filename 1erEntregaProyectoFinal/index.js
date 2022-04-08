89f7ee801df39414d4ab8f37e739cdc9e3027b28
hugozepeda18 <a01228679@exatec.tec.mx>
2022-04-07 19:47:27 -0500

    Proyecto Final Parte 1

--- 1erEntregaProyectoFinal/index.js
+++ 1erEntregaProyectoFinal/index.js
@@ -1,23 +1,16 @@
 const express = require("express")
 const apiRouter = require("./api")
-const path = require("path")
-const bodyParse = require("body-parser")
 
 const PORT = 8080
 const app = express()
 
 app.use(express.json())
-app.use(bodyParse.urlencoded({ extended: false}))
 app.use("/api", apiRouter)
 
 app.use((req, res, next) => {
     let metodo = req.method
     let ruta = req.path
-    res.send({error: -2, descripcion: `ruta ${ruta}, metodo ${metodo} no implementada`}, 404)
-})
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.status(404).send({error: -2, descripcion: `ruta ${ruta}, metodo ${metodo} no implementada`})
 })
 
 app.listen(PORT, () => {
